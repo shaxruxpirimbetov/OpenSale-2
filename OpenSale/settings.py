8db0d2aaffc9e0314c1c4f3500d4e11a20a20bef
shaxruxpirimbetov <shaxruxpirimbetov@gmail.com>
2026-08-12 20:52:09 +0500

    fix

--- OpenSale/settings.py
+++ OpenSale/settings.py
@@ -63,12 +63,23 @@ WSGI_APPLICATION = 'OpenSale.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+DATABASE_URL = config("DATABASE_URL", default="")
+
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.parse(
+            DATABASE_URL,
+            conn_max_age=600,
+            ssl_require=True,
+        )
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
